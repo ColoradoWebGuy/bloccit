4b3d309865a2dbc82053d867de47fcaca7cb148a
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-08 15:43:15 -0600

    Assignment 31 completed, CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    #overwrite the title of every fifth instance of Post with the text "CENSORED".
+    @posts.each { |x|
+      x.title = "CENSORED" if x.id % 5 == 0
+    }
   end
 
   def show
