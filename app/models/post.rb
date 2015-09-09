4e7dd3fa8c74d0ded94770e12664ff97caafd577
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-09 10:48:15 -0600

    Assignment 31 completed: Updated the solution to overwrite the title of every fifth instance of Post with the text CENSORED - using the Model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  #overwrite the title of every fifth instance of Post with the text "CENSORED".
+  def self.censored
+    all.each { |post|
+      post.title = "CENSORED" if post.id % 5 == 0
+    }
+  end
+
 end
