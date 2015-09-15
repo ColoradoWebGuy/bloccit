b9d096ce5d13e24f7c3debc86d06cf424e41659e
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-15 09:50:50 -0600

    Checkpoint 38 completed, Authentication: Sign In

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
   resources :topics do
      resources :posts, except: [:index]
   end
+
   resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
 
