4653e0d76364c3a6357bb6d2001cadc7deef4bdc
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-15 07:57:55 -0600

    Assignment 37 completed, Authentication: Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@
   resources :topics do
      resources :posts, except: [:index]
   end
+
   resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
