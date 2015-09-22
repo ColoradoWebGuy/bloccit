94d6ea3602c57d9a3fba0b00d25dff431fc53a47
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-22 17:15:35 -0600

    Checkpoint 44 completed, Favorites

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,7 +14,8 @@ RSpec.describe Post, type: :model do
   it { should have_many(:labels).through(:labelings) }
 
   it { should have_many(:votes) }
-
+  it { should have_many(:favorites) }
+  
   it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
