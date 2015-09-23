625bb51908791cc3b1064588638e9fd00d393a34
Steven Schulte <shotgunsteven4@yahoo.com>
2015-09-23 15:52:21 -0600

    Checkpoint 45 completed, Profiles

--- Gemfile
+++ Gemfile
@@ -34,3 +34,5 @@ source 'https://rubygems.org'
  gem 'bcrypt'
 
  gem 'figaro', '1.0'
+
+ gem 'factory_girl_rails', '~> 4.0'
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,9 @@ include RandomData
 
 RSpec.describe Post, type: :model do
 
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+  let(:topic) { create(:topic) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
 
   # validations
   it { should have_many(:comments) }
@@ -15,7 +15,7 @@ RSpec.describe Post, type: :model do
 
   it { should have_many(:votes) }
   it { should have_many(:favorites) }
-  
+
   it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
